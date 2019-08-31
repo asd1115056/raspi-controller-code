07326ba2e95deee8021a28a7d7677437f855f728
柏翰 黃 <asd1115056@gmail.com>
2019-08-31 16:00:01 +0800

    def Add_Scheduler(x)

--- json_con.py
+++ json_con.py
@@ -5,8 +5,12 @@ new=[{"Tag": "FDLMCW", "food_Name": "tamto", "schedule_time": "08:00:00", "food_
 "food_Name": "tamto", "schedule_time": "11:37:00", "food_amount": "30.00"}]
 new1=[{"Tag": "FDLMCW", "food_Name": "tamto", "schedule_time": "08:00:00", "food_amount": "50.00"}, {"Tag": "EAVBR1", "food_Name": "tamto", "schedule_time": "09:00:00", "food_amount": "50.00"}, {"Tag": "EAVBR1", "food_Name": "tamto", "schedule_time": "11:34:00", "food_amount": "50.00"}, {"Tag": "EAVBR1", "food_Name": "tamto", "schedule_time": "11:35:00", "food_amount": "50.00"}, {"Tag": "FDLMCW", "food_Name": "tamto", "schedule_time": "11:36:00", "food_amount": "50.00"}, {"Tag": "EAVBR1", 
 "food_Name": "tamto", "schedule_time": "11:37:00", "food_amount": "30.00"}]
+file_name='Scheduler_save.text'
 with open('Scheduler_save.text','r') as f:
     output=eval(f.readline())
+def read_file(x):
+    with open(x,'r') as f:    
+        return eval(f.readline())
 # output=json.dumps(output)
 #output=json.loads(output)
 #output=demjson.encode(output)
@@ -14,11 +18,13 @@ with open('Scheduler_save.text','r') as f:
 # output = output.replace("'", '"') 
 #output = list(output)
 #new=json.loads(new)
-print(output)
+print(read_file(file_name))
+'''
 print(type(output))
 if output==new:
     print("Yes")
 else:
     print("No")
+'''
 #print(output)
 #print(new)

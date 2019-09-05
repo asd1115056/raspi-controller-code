1e76399c3fe3f5b91bd9ebd3f9534dadf4d0fb81
柏翰 黃 <asd1115056@gmail.com>
2019-09-05 19:09:48 +0800

    project updata

--- main code/project.py
+++ main code/project.py
@@ -12,7 +12,7 @@ from bluepy.btle import *
 
 
 sys.modules['smbus'] = smbus2
-lcd = CharLCD('PCF8574', address=0x27, port=1, backlight_enabled=False)
+lcd = CharLCD('PCF8574', address=0x27, port=1, backlight_enabled=True)
 url1 = 'http://localhost:8000/ajax/all_list_Schedule'
 suuid = UUID(0xffe0)
 cuuid = UUID(0xffe1)
@@ -27,25 +27,30 @@ class MyDelegate(DefaultDelegate):
 
     def handleNotification(self, cHandle, data):
         global temp
-        temp=data.decode("ascii")
-        # ... perhaps check cHandle
-        # ... process 'data'
+        try:
+            temp=data.decode("UTF-8")
+        except UnicodeError:
+            None
 
-def Notify():
-    p = Peripheral(mac)
-    p.setDelegate(MyDelegate())
-    print("connected")
-    u = p.getServiceByUUID(suuid)
-    ch = u.getCharacteristics(cuuid)[0]
-    desc = ch.getDescriptors()[0]
-    ch.write(bytes("t","ascii"))
-    desc.write(b"\x01\x00", True)
-    print("writing done")
-    p.waitForNotifications(1.0)
-
-def ble_initialization():
+def ble_initializing():
     statue=True
     while statue:
+        dot=3
+        lcd.clear()
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string("BLE Initializing")
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string("BLE            ")
+        while dot>-1:
+            lcd.cursor_pos = (1, 3)
+            if dot==2:
+                lcd.write_string(".")
+            if dot==1:
+                lcd.write_string("..")
+            if dot==0:
+                lcd.write_string("...")
+            time.sleep(1)
+            dot-=1
         try:
             p = Peripheral(mac)
             p.setDelegate(MyDelegate())
@@ -53,25 +58,74 @@ def ble_initialization():
             ch = u.getCharacteristics(cuuid)[0]
             desc = ch.getDescriptors()[0]
             desc.write(b"\x01\x00", True)
-            ch.write(bytes("t","ascii"))
+            ch.write(bytes("t","UTF-8"))
             if p.waitForNotifications(1.0):
                 if temp=='ok':
                     statue=False
-                    print("skip")
+                    lcd.cursor_pos = (1, 7)
+                    lcd.write_string("OK!")
+                    time.sleep(3)
+                else:
+                    lcd.cursor_pos = (1, 7)
+                    lcd.write_string("FAIL!")
+                    time.sleep(3)
+            p.disconnect()
         except BTLEException:
-            print("fail:wait for 40 sec")
+            status=True
+            lcd.cursor_pos = (1, 7)
+            lcd.write_string("FAIL!")
+            print("error")
+            time.sleep(3)
+        finally:
+            lcd.clear()
 
-def net_initialization():
+def net_initializing():
     statue=True
     global url1
     while statue:
+        timer=30
+        dot=4
+        lcd.clear()
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string("Net Initializing")
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string("Net")
+        while dot>0:
+            lcd.cursor_pos = (1, 3)
+            if dot==3:
+              lcd.write_string(".")
+            if dot==2:
+              lcd.write_string("..")
+            if dot==1:
+              lcd.write_string("...")
+            time.sleep(1)
+            dot-=1
         try:
             response = requests.get(url1)
             response.raise_for_status()
+            lcd.cursor_pos = (1, 7)
+            lcd.write_string("OK!")
+            time.sleep(3)
             statue=False
         except requests.exceptions.RequestException:
-            print("fail:wait for 40 sec")
-            time.sleep(40)
+            lcd.cursor_pos = (1, 7)
+            lcd.write_string("FAIL!")
+            time.sleep(3)
+            lcd.cursor_pos = (1, 0)
+            lcd.write_string("Retry After")
+            while timer>-1:
+                lcd.cursor_pos = (1,13)
+                lcd.write_string("  s")
+                if timer<10:
+                    lcd.cursor_pos = (1,14)
+                else:
+                    lcd.cursor_pos = (1,13)
+                lcd.write_string(str(timer))
+                #print(timer)
+                timer-=1
+                time.sleep(1)
+        finally:
+            lcd.clear()
 
 def sync_time():
     try:
@@ -149,5 +203,7 @@ def sync():
 
 if __name__ == "__main__":
     sync_time()
+    net_initializing()
+    ble_initializing()
     sched.add_job(sync, 'interval', seconds=5)
     sched.start()

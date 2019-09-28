302c4953cf0be130210ddf7a86e02ad86646099c
柏翰 黃 <asd1115056@gmail.com>
2019-09-29 00:09:24 +0800

    upload

--- main code/upload.py
+++ main code/upload.py
@@ -1,7 +1,7 @@
 import json
 import requests
 url = "http://localhost:8000/json_upload"
-params = {'Tag':'KFJM142','water_drink':1512,'food_eat':132} 
+params = {'Tag':'KFJM142','water_drink':112,'food_eat':1132,'active_time':'2019-09-23 11:56:00'} 
 headers = {'content-type': 'application/json'}
 response = requests.post(url,data=json.dumps(params),headers=headers)
 print(response)

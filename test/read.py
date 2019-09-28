12b957ac177ef356fa3ccdd6bdb0f898b126ec69
柏翰 黃 <asd1115056@gmail.com>
2019-09-28 21:34:06 +0800

    j2000 to utc+8

--- test/read.py
+++ test/read.py
@@ -1,10 +1,12 @@
 import time
-fp = open('file.txt', "r")
+import datetime
+fp = open('env.txt', "r")
 line = fp.readline()
  
 ## 用 while 逐行讀取檔案內容，直至檔案結尾
 while line:
-    print (line)
+    #print (line[2:11])
+    print(datetime.datetime(2000, 1, 1, 12, 0) + datetime.timedelta(seconds=int(line[2:11])))
     line = fp.readline()
     time.sleep(1)
  

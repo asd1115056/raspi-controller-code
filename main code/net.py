6b638666e5ae3ccaaa0d60f2001203cd9a071984
柏翰 黃 <asd1115056@gmail.com>
2019-10-11 22:35:53 +0800

    download_schedule net.py

--- main code/net.py
+++ main code/net.py
@@ -4,19 +4,19 @@ import json
 
 upload_url = 'http://192.168.0.3:8000/api/data_upload'
 download_url = 'http://localhost:8000/ajax/all_list_Schedule'
-url='https://www.google.com.tw/webhp?hl=zh-TW'
+test_url='https://www.google.com.tw/webhp?hl=zh-TW'
 
 params1 = "E111585004FCC6224266200240005400"
 params2 = "P111585004FCC6224266206D1CDF2B0988700000"
 
 def net_initializing():
     #check django Server Satute
-    global url
+    global test_url
     lcd_clearall()
     lcd_print(0,0,0.5,"Net Initializing")
     lcd_print(1,0,2,"Connecting.....    ")
     try:
-        response = requests.get(url,timeout=20)
+        response = requests.get(test_url,timeout=20)
         response.raise_for_status()
         lcd_print(1,0,1,"SUCCESS!        ")
         return True
@@ -26,19 +26,24 @@ def net_initializing():
     finally:
         lcd_clearall()
 
-def upload(url,text):
+def upload_data(url,text):
     try:
         params = {'DATA': text}
         headers = {'content-type': 'application/json'}
         response = requests.post(url, data=json.dumps(params), headers=headers,timeout=20)
-        if response.status_code == 200:
-            return True
-        else:
-            return False
+        response.raise_for_status()
+        return True
         #print(response.text)
     except requests.exceptions.RequestException as e:
         return False
 
+def download_schedule(url,text):
+    try:
+        response = requests.get(url,timeout=20)
+        response.raise_for_status()
+        return response.text
+    except requests.exceptions.RequestException as e:
+        return False
 
         
 
@@ -47,6 +52,5 @@ if __name__ == "__main__":
     bool1=net_initializing(url)
     print(bool1)
     '''
-
-    a=upload(upload_url,params2)
+    a=upload_data(upload_url,params2)
     print(a)
